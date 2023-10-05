add0e0edb8b3c58f94d9f0d20bff39dfd74315e8
Mrinal Patra <96857582+mrinal2002@users.noreply.github.com>
2023-10-05 23:52:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const fs = require("fs");
+app.use(express.static("public"));
+app.use(express.urlencoded());
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"Products.json"));
+})
+
+app.get("/category/:category", (req, res) => {
+     fs.readFile("Products.json", "utf-8", (err, data) => {
+       const products = JSON.parse(data);
+       const newdata=req.params.category;
+       console.log(newdata)
+       let result=products.filter((item)=>{
+        if(item.Category==newdata){
+            return true;
+        }
+       })
+       res.send(result);
+    })
+})
+
+app.listen(3001, (err) => {
+    if (err) {
+        console.log("unable to start!!");
+    }
+    else {
+        console.log("server started..");
+    }
+})
